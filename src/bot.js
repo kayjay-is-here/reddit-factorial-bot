7f71e64110a5c5071a6f6bce72f5b4ccd65548a2
password12004 <yoko.nasana@gmail.com>
2020-09-02 11:13:04 -0700

    added scientific notation
    someone should probably fix double factorial

--- src/bot.js
+++ src/bot.js
@@ -4,9 +4,10 @@ try {
   var factorial = (n) => Array.from({
     length: n
   }, (v, k) => k + 1).reduce((a, b) => a * b, 1)
+  //TODO  this might not be correct
   var double_factorial = (n) => Array.from({
     length: n
-  }, (v, k) => k + 1).reduce((a, b) => a * b, 2)
+  }, (v, k) => k + 2).reduce((a, b) => a * b, 1)
   // TODO host on heroku
   const r = new Snoowrap({userAgent: 'factorialbot (v1, kjl3080)', clientId: config.clientId, clientSecret: config.clientSecret, username: config.username, password: config.password});
 
@@ -25,11 +26,11 @@ try {
           let matched = isFac.match(comment)
           let doubleMatched = isDoubleFac.match(comment)
           if (doubleMatched !== null) {
-            return c.reply(`Are you sure you meant \`${matched[0]}\`? Because that would be equal to \`${double_factorial(parseInt(matched[0].substring(0, matched[0].length - 1)))}\`.
+            return c.reply(`Are you sure you meant \`${matched[0]}\`? Because that would be equal to \`${double_factorial(parseInt(matched[0].substring(0, matched[0].length - 1))).toExponential(7)}\`.
             \nAlso, this is a double factorial, so it's less.\n^this ^bot ^ was ^ made ^by ^u/kjl3080. `);
           }
           if (matched !== null) {
-            return c.reply(`Are you sure you meant \`${matched[0]}\`? Because that would be equal to \`${factorial(parseInt(matched[0].substring(0, matched[0].length - 1)))}\`.
+            return c.reply(`Are you sure you meant \`${matched[0]}\`? Because that would be equal to \`${factorial(parseInt(matched[0].substring(0, matched[0].length - 1))).toExponential(7)}\`.
             \n^this ^bot ^ was ^ made ^by ^u/kjl3080.`)
           }
         })
